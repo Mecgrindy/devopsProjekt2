7a88882a0d3e3f2fdf790234f5de86a59b0f2293
Gracijan <grindy2015@gmail.com>
2017-12-07 00:45:52 +0100

    Hello endpointok is.

--- src/main/java/com/example/sum/hello/HelloController.java
+++ src/main/java/com/example/sum/hello/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.sum.hello;
 
+import java.io.File;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
   @RequestMapping("/")
-  String home(@RequestParam(value = "name", defaultValue = "Hello Pityi Palko") String name) {
-    return name;
+  String home(@RequestParam(value = "name", defaultValue = "Pityi Palko") String name) {
+    return "Hello" + name;
+  }
+
+  @RequestMapping("/endpoints")
+  String endpoints() {
+    return "url: /, param: String name, default: Pityi Palko <br>" + "url: /lower, param: String word <br>"
+        + "url: /upper, param: String word <br>" + "url: /reverse, param: String word <br>"
+        + "url: /add, param: int nmb1, int nmb2 <br>" + "url: /minus, param: int nmb1, int nmb2<br>"
+        + "url: /divide, param: int nmb1, int nmb2 <br>" + "url: /multiply, param: int nmb1, int nmb2 <br>"
+        + "url: /hex, param: String colorName <br>" + "url: /rgb, param: String colorName";
+  }
+
+  @RequestMapping("/lower")
+  String lower(@RequestParam(value = "word") String word) {
+    return word.toLowerCase();
+  }
+
+  @RequestMapping("/upper")
+  String upper(@RequestParam(value = "word") String word) {
+    return word.toUpperCase();
+  }
+
+  @RequestMapping("/reverse")
+  String reverse(@RequestParam(value = "word") String word) {
+    char[] in = word.toCharArray();
+    int begin = 0;
+    int end = in.length - 1;
+    char temp;
+    while (end > begin) {
+      temp = in[begin];
+      in[begin] = in[end];
+      in[end] = temp;
+      end--;
+      begin++;
+    }
+    return new String(in);
   }
 
 }
