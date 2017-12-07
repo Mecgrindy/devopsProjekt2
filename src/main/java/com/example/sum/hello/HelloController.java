c8bb67d9af35a5852b37e096c07caa8c5ed7d6af
Gracijan <grindy2015@gmail.com>
2017-12-07 01:19:37 +0100

    Html css linkek minden iranyban.

--- src/main/java/com/example/sum/hello/HelloController.java
+++ src/main/java/com/example/sum/hello/HelloController.java
@@ -11,16 +11,22 @@ public class HelloController {
 
   @RequestMapping("/")
   String home(@RequestParam(value = "name", defaultValue = "Pityi Palko") String name) {
-    return "Hello" + name;
+    return "<div style='position:fixed; top:0; left:0; width:100vw; height: 100vh; background-color:#263238; color:white; text-align:center;'><h1>"
+        + "Hello " + name
+        + "</h1><h3><b>Mindenes alkalmazás</b> <small style='color: silver'>Devops Projekt</small></h3><h4><a href='/endpoints'>Végpontok</a></h4>"
+        + "<p>A többi végpont kinézete nem lett formázva a tesztek miatt.</p>"
+        + "<h3 style='color:#00E5FF'><b>by: Torma Roland és Kokrehel Grácián</b></h3></div>";
   }
 
   @RequestMapping("/endpoints")
   String endpoints() {
-    return "url: /, param: String name, default: Pityi Palko <br>" + "url: /lower, param: String word <br>"
-        + "url: /upper, param: String word <br>" + "url: /reverse, param: String word <br>"
-        + "url: /add, param: int nmb1, int nmb2 <br>" + "url: /minus, param: int nmb1, int nmb2<br>"
-        + "url: /divide, param: int nmb1, int nmb2 <br>" + "url: /multiply, param: int nmb1, int nmb2 <br>"
-        + "url: /hex, param: String colorName <br>" + "url: /rgb, param: String colorName";
+    return "<a href='/?name=Lacika'>Name Lacika</a><br>"
+        + "<a href='/lower?word=DSGSVCXfsFDSF'>Lower DSGSVCXfsFDSF</a><br>"
+        + "<a href='/upper?word=jsdkfgdvjkhdb'>Upper jsdkfgdvjkhdb</a><br>"
+        + "<a href='/reverse?word=Ujra vad a varju'>Reverse Ujra vad a varju</a><br>"
+        + "<a href='/add?nmb1=2&nmb2=3'>2+3</a><br>" + "<a href='/minus?nmb1=10&nmb2=4'>10-4</a><br>"
+        + "<a href='/divide?nmb1=25&nmb2=5'>25/5</a><br>" + "<a href='/multiply?nmb1=3&nmb2=7'>3*7</a><br>"
+        + "<a href='/hex?colorName=red'>Hex red</a><br>" + "<a href='/rgb?colorName=orange'>RGB orange</a><br>";
   }
 
   @RequestMapping("/lower")
